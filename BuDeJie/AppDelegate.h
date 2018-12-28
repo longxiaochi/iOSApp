1bc2de4e6e8d4a509dbbc11df9f4208f056e725b
longchi <1345618834@qq.com>
2018-12-28 21:09:14 +0800

    Initial Commit

---
+++ BuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJie
+//
+//  Created by LongMac on 2018/12/28.
+//  Copyright © 2018年 LongMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
